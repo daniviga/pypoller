46d869ac53845f99138add0158aa003b741100f4
Daniele Viganò <daniele@vigano.me>
2020-11-02 15:28:59 +0100

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pypoller',
-    version='1.0.0',
+    version='0.1.0',
     python_requires='>=3.5',
     install_requires=['pymodbus'],
     scripts=['pypoller.py'],
