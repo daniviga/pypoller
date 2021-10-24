0e62981a396c9c91b79b381d7c7d26b0ba3275cb
Daniele Viganò <daniele@vigano.me>
2021-10-24 12:36:07 +0200

    v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pypoller',
-    version='0.2.0',
+    version='0.2.1',
     python_requires='>=3.7',
     install_requires=['pymodbus'],
     scripts=['pypoller.py'],
